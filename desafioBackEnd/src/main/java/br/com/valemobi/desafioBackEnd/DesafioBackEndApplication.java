5982c8bbb05b7b670658d0f9b6fbfcbe898537c4
Eduardo <eduardoc313@gmail.com>
2021-04-09 11:27:32 -0300

    Adicionando o projeto

---
+++ desafioBackEnd/src/main/java/br/com/valemobi/desafioBackEnd/DesafioBackEndApplication.java
@@ -0,0 +1,13 @@
+package br.com.valemobi.desafioBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBackEndApplication.class, args);
+	}
+
+}
---
+++ desafioBackEnd/src/test/java/br/com/valemobi/desafioBackEnd/DesafioBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.valemobi.desafioBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
