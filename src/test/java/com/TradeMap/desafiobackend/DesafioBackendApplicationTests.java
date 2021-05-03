e6987fb7d0a16ece21c5d8d67da53eb94ab62f53
jojoccp <jonatas.ccp@gmail.com>
2021-05-03 17:17:18 -0300

    initial commit

---
+++ src/test/java/com/TradeMap/desafiobackend/DesafioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TradeMap.desafiobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
